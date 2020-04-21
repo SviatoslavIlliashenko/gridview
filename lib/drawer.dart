cfd7f4fd6805ac4c4db1ff7c8929c1ddd6f4bcda
SviatoslavIlliashenko <Sviatoslav.illiashenko@nure.ua>
2020-04-21 13:28:56 +0200

    second commit

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -7,6 +7,8 @@ class SettingsPage extends StatefulWidget {
   _SettingsPageState createState() => _SettingsPageState();
 }
 
+
+
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
